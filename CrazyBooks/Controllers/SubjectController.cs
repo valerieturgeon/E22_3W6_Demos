f1fa19ef7b64ee4251a9382c7f24d563138f7474
Valerie.turgeon <valerie.turgeon@cegepmontpetit.ca>
2022-06-01 07:13:14 -0400

    FCT Visualiser et ajouter Subject
    Ajout du modèle Subject, du Controller et des Actions/Views Index et Create pour Subject

---
+++ CrazyBooks/Controllers/SubjectController.cs
@@ -0,0 +1,42 @@
+﻿using CrazyBooks.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrazyBooks.Controllers
+{
+  public class SubjectController : Controller
+  {
+    public IActionResult Index()
+    {
+      this.ViewBag.MaListe = new List<Subject>()
+      {
+        new Subject(){Name= "Thriller", Id=1},
+        new Subject(){Name= "Biographie", Id=2},
+        new Subject(){Name= "Drame", Id=3},
+        new Subject(){Name= "Ressources humaines", Id=4}
+      };
+      return View();
+    }
+
+    //GET CREATE
+    public IActionResult Create()
+    {
+      return View();
+    }
+
+    //POST CREATE
+    [HttpPost]
+    public IActionResult Create(Subject subject)
+    {
+      if (ModelState.IsValid)
+      {
+        // Ajouter à la BD
+      }
+
+      return this.View(subject);
+    }
+  }
+}
