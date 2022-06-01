f06b3986b0dee88114e44802f64465f6e731340f
Valerie.turgeon <valerie.turgeon@cegepmontpetit.ca>
2022-06-01 07:15:23 -0400

    FCT Visualiser et ajouter Book
    Ajout du modèle Book, du Controller et des actions/Views Index et Create pour Book

---
+++ CrazyBooks/Controllers/BookController.cs
@@ -0,0 +1,42 @@
+﻿using CrazyBooks.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrazyBooks.Controllers
+{
+  public class BookController : Controller
+  {
+    public IActionResult Index()
+    {
+      this.ViewBag.MaListe = new List<Book>()
+      {
+        new Book(){ Id=1, Title= "Cobayes", ISBN= "9782896623921"},
+        new Book(){Id=2, Title= "Enlèvement", ISBN= "9782896626151" },
+        new Book(){Id=3, Title= "Le chiffreur", ISBN= "9782890747364" },
+        new Book(){Id=4, Title= "Les Maudits", ISBN= "9782896628773"}
+      };
+      return View();
+    }
+
+    //GET CREATE
+    public IActionResult Create()
+    {
+      return View();
+    }
+
+    //POST CREATE
+    [HttpPost]
+    public IActionResult Create(Book book)
+    {
+      if (ModelState.IsValid)
+      {
+        // Ajouter à la BD
+      }
+
+      return this.View(book);
+    }
+  }
+}
