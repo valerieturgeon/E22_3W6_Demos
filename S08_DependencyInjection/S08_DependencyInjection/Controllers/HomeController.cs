27a94b6590e90eebaf3ae436796a808a69780bf4
Dominic Proulx <dominic.proulx@cegepmontpetit.ca>
2022-06-09 13:12:01 -0400

    FCT - Ajout du ServiceAbc dans le controller

--- S08_DependencyInjection/S08_DependencyInjection/Controllers/HomeController.cs
+++ S08_DependencyInjection/S08_DependencyInjection/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Logging;
 using S08_DependencyInjection_Models;
 using S08_DependencyInjection_Models.ViewModels;
+using S08_DependencyInjection_Services.Interfaces;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace S08_DependencyInjection.Controllers
@@ -9,15 +11,19 @@ namespace S08_DependencyInjection.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IServiceAbc _svcAbc;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IServiceAbc svcAbc)
         {
             _logger = logger;
+            _svcAbc = svcAbc;
         }
 
         public IActionResult Index()
         {
-            return View(new HomeIndexVM());
+            _svcAbc.DoSomething();
+
+            return View(new HomeIndexVM(new List<MonitoringServiceInfosVM> { _svcAbc.GetInfos }));
         }
 
         public IActionResult Privacy()
