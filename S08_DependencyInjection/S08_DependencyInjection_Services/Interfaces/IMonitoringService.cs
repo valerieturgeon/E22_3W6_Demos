9cd1fd8f39d4091cda0066a0e25750778fb8f193
Dominic Proulx <dominic.proulx@cegepmontpetit.ca>
2022-06-09 12:47:55 -0400

    FCT - Ajout des interfaces des services utilisés

---
+++ S08_DependencyInjection/S08_DependencyInjection_Services/Interfaces/IMonitoringService.cs
@@ -0,0 +1,10 @@
+﻿using S08_DependencyInjection_Models.ViewModels;
+
+namespace S08_DependencyInjection_Services.Interfaces
+{
+    public interface IMonitoringService
+    {
+        MonitoringServiceInfosVM GetInfos { get; }
+        void DoSomething();
+    }
+}
