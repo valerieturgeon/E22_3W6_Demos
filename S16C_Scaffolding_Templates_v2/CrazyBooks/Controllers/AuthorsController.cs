15b5b2a61884121299cb01666bb8988183f18b6a
Dominic Proulx <dominic.proulx@cegepmontpetit.ca>
2022-06-19 19:41:26 -0400

    REFACTOR - Gestion des Authors

--- S16C_Scaffolding_Templates_v2/CrazyBooks/Controllers/AuthorsController.cs
+++ S16C_Scaffolding_Templates_v2/CrazyBooks/Controllers/AuthorsController.cs
@@ -1,8 +1,11 @@
 ﻿using CrazyBooks_DataAccess.Data;
 using CrazyBooks_Models.Models;
+using CrazyBooks_Models.ViewModels;
+using CrazyBooks_Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,8 +23,16 @@ namespace CrazyBooks.Controllers
         // GET: Authors
         public async Task<IActionResult> Index()
         {
-            var crazyBooksDbContext = _db.Authors.Include(a => a.AuthorDetail);
-            return View(await crazyBooksDbContext.ToListAsync());
+            return View(
+                    new AuthorsIndexVM(
+                        "Authors",
+                        "Authors",
+                        new List<PageLinks>() { PageLinks.Create },
+                        await _db.Authors
+                            .Include(a => a.AuthorDetail)
+                            .ToListAsync()
+                    )
+            );
         }
 
         // GET: Authors/Details/5
@@ -32,92 +43,103 @@ namespace CrazyBooks.Controllers
                 return NotFound();
             }
 
-            var author = await _db.Authors
-                .Include(a => a.AuthorDetail)
-                .FirstOrDefaultAsync(m => m.Id == id);
+            Author author = await _db.Authors
+                                                
+.Include(a => a.AuthorDetail)
+                                                    .FirstOrDefaultAsync(e => e.Id == id);
             if (author == null)
             {
                 return NotFound();
             }
 
-            return View(author);
+            return View(
+                    "Display",
+                    new AuthorsDisplayVM(
+                        true,
+                        "Author",
+                        "Author",
+                        new List<PageLinks>() { PageLinks.BackToList, PageLinks.Edit },
+                        author
+                    )
+            );
         }
 
-        // GET: Authors/Create
-        public IActionResult Create()
+        // GET: Authors/Upsert/5
+        public async Task<IActionResult> Upsert(int? id)
         {
-            ViewData["AuthorDetail_Id"] = new SelectList(_db.AuthorsDetail, "Id", "Id");
-            return View();
+            bool isCreate = id == null;
+            Author author = null;
+
+            if (!isCreate)
+            {
+                // Extra stuff for Edit
+                author = await _db.Authors.FirstOrDefaultAsync(e => e.Id == id);
+                if (author == null)
+                {
+                    return NotFound();
+                }
+            }
+
+            return View(
+                GetAuthorsUpsertVM(
+                    isCreate, 
+                    new Dictionary<string,SelectList­>(){
+                        { "ListForAuthorDetail_Id", new SelectList(_db.AuthorsDetail, "Id", "Id") },
+                    }, 
+                    author
+                )
+            );
         }
 
-        // POST: Authors/Create
+        // POST: Authors/Upsert
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,AuthorDetail_Id")] Author author)
+        public async Task<IActionResult> Upsert(AuthorsUpsertVM vm)
         {
+            if(vm.IsCreate)
+            {
+                ModelState.Remove("Author.Id");
+            }
+
             if (ModelState.IsValid)
             {
-                _db.Add(author);
+                if (vm.IsCreate)
+                {
+                    _db.Add(vm.Author);
+                }
+                else
+                {
+                    try
+                    {
+                        _db.Update(vm.Author);
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!AuthorExists(vm.Author.Id))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                }
                 await _db.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["AuthorDetail_Id"] = new SelectList(_db.AuthorsDetail, "Id", "Id", author.AuthorDetail_Id);
-            return View(author);
-        }
 
-        // GET: Authors/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var author = await _db.Authors.FindAsync(id);
-            if (author == null)
-            {
-                return NotFound();
-            }
-            ViewData["AuthorDetail_Id"] = new SelectList(_db.AuthorsDetail, "Id", "Id", author.AuthorDetail_Id);
-            return View(author);
-        }
-
-        // POST: Authors/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,AuthorDetail_Id")] Author author)
-        {
-            if (id != author.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _db.Update(author);
-                    await _db.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AuthorExists(author.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["AuthorDetail_Id"] = new SelectList(_db.AuthorsDetail, "Id", "Id", author.AuthorDetail_Id);
-            return View(author);
+            return View(
+                    GetAuthorsUpsertVM(
+                        vm.IsCreate, 
+                        new Dictionary<string,SelectList­>(){
+                        { "ListForAuthorDetail_Id", new SelectList(_db.AuthorsDetail, "Id", "Id") },
+                        }, 
+                        vm.Author
+                    )
+            );
         }
 
         // GET: Authors/Delete/5
@@ -128,15 +150,26 @@ namespace CrazyBooks.Controllers
                 return NotFound();
             }
 
-            var author = await _db.Authors
-                .Include(a => a.AuthorDetail)
-                .FirstOrDefaultAsync(m => m.Id == id);
+            Author author = await _db.Authors
+                                                
+.Include(a => a.AuthorDetail)
+                                                    .FirstOrDefaultAsync(e => e.Id == id);
             if (author == null)
             {
                 return NotFound();
             }
 
-            return View(author);
+            return View(
+                    "Display",
+                    new AuthorsDisplayVM(
+                        false,
+                        "Author",
+                        "Are you sure you want to delete this Author",
+                        new List<PageLinks>() { PageLinks.BackToList },
+                        author,
+                        "Supprimer"
+                    )
+            );
         }
 
         // POST: Authors/Delete/5
@@ -144,7 +177,7 @@ namespace CrazyBooks.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var author = await _db.Authors.FindAsync(id);
+            Author author = await _db.Authors.FindAsync(id);
             _db.Authors.Remove(author);
             await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
@@ -153,6 +186,19 @@ namespace CrazyBooks.Controllers
         private bool AuthorExists(int id)
         {
             return _db.Authors.Any(e => e.Id == id);
+        }
+
+        private AuthorsUpsertVM GetAuthorsUpsertVM(bool isCreate, Dictionary<string,SelectList­> selectLists, Author author = null)
+        {
+            return new AuthorsUpsertVM(
+                        isCreate,
+                        "Author",
+                        "Author",
+                        new List<PageLinks>() { PageLinks.BackToList },
+                        isCreate ? "Ajouter" : "Modifier",
+                        selectLists,
+                        author
+            );
         }
     }
 }
