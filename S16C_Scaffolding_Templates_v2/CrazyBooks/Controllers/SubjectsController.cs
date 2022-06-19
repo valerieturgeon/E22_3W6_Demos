5488b53ffe19b5baf2971dfdadc432efdb38f4a3
Dominic Proulx <dominic.proulx@cegepmontpetit.ca>
2022-06-19 19:41:40 -0400

    REFACTOR - Gestion des Subjects

--- S16C_Scaffolding_Templates_v2/CrazyBooks/Controllers/SubjectsController.cs
+++ S16C_Scaffolding_Templates_v2/CrazyBooks/Controllers/SubjectsController.cs
@@ -1,9 +1,14 @@
-﻿using CrazyBooks_DataAccess.Data;
-using CrazyBooks_Models.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CrazyBooks_DataAccess.Data;
+using CrazyBooks_Models.Models;
+using CrazyBooks_Models.ViewModels;
+using CrazyBooks_Utility;
 
 namespace CrazyBooks.Controllers
 {
@@ -19,7 +24,16 @@ namespace CrazyBooks.Controllers
         // GET: Subjects
         public async Task<IActionResult> Index()
         {
-            return View(await _db.Subjects.ToListAsync());
+            return View(
+                    new SubjectsIndexVM(
+                        "Subjects",
+                        "Subjects",
+                        new List<PageLinks>() { PageLinks.Create },
+                        await _db.Subjects
+                            
+                            .ToListAsync()
+                    )
+            );
         }
 
         // GET: Subjects/Details/5
@@ -30,87 +44,100 @@ namespace CrazyBooks.Controllers
                 return NotFound();
             }
 
-            var subject = await _db.Subjects
-                .FirstOrDefaultAsync(m => m.Id == id);
+            Subject subject = await _db.Subjects
+                                                
+                                                    .FirstOrDefaultAsync(e => e.Id == id);
             if (subject == null)
             {
                 return NotFound();
             }
 
-            return View(subject);
+            return View(
+                    "Display",
+                    new SubjectsDisplayVM(
+                        true,
+                        "Subject",
+                        "Subject",
+                        new List<PageLinks>() { PageLinks.BackToList, PageLinks.Edit },
+                        subject
+                    )
+            );
         }
 
-        // GET: Subjects/Create
-        public IActionResult Create()
+        // GET: Subjects/Upsert/5
+        public async Task<IActionResult> Upsert(int? id)
         {
-            return View();
+            bool isCreate = id == null;
+            Subject subject = null;
+
+            if (!isCreate)
+            {
+                // Extra stuff for Edit
+                subject = await _db.Subjects.FirstOrDefaultAsync(e => e.Id == id);
+                if (subject == null)
+                {
+                    return NotFound();
+                }
+            }
+
+            return View(
+                GetSubjectsUpsertVM(
+                    isCreate, 
+                    new Dictionary<string,SelectList­>(){
+                    }, 
+                    subject
+                )
+            );
         }
 
-        // POST: Subjects/Create
+        // POST: Subjects/Upsert
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] Subject subject)
+        public async Task<IActionResult> Upsert(SubjectsUpsertVM vm)
         {
+            if(vm.IsCreate)
+            {
+                ModelState.Remove("Subject.Id");
+            }
+
             if (ModelState.IsValid)
             {
-                _db.Add(subject);
+                if (vm.IsCreate)
+                {
+                    _db.Add(vm.Subject);
+                }
+                else
+                {
+                    try
+                    {
+                        _db.Update(vm.Subject);
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        if (!SubjectExists(vm.Subject.Id))
+                        {
+                            return NotFound();
+                        }
+                        else
+                        {
+                            throw;
+                        }
+                    }
+                }
                 await _db.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(subject);
-        }
 
-        // GET: Subjects/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var subject = await _db.Subjects.FindAsync(id);
-            if (subject == null)
-            {
-                return NotFound();
-            }
-            return View(subject);
-        }
-
-        // POST: Subjects/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Subject subject)
-        {
-            if (id != subject.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _db.Update(subject);
-                    await _db.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!SubjectExists(subject.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(subject);
+            return View(
+                    GetSubjectsUpsertVM(
+                        vm.IsCreate, 
+                        new Dictionary<string,SelectList­>(){
+                        }, 
+                        vm.Subject
+                    )
+            );
         }
 
         // GET: Subjects/Delete/5
@@ -121,14 +148,25 @@ namespace CrazyBooks.Controllers
                 return NotFound();
             }
 
-            var subject = await _db.Subjects
-                .FirstOrDefaultAsync(m => m.Id == id);
+            Subject subject = await _db.Subjects
+                                                
+                                                    .FirstOrDefaultAsync(e => e.Id == id);
             if (subject == null)
             {
                 return NotFound();
             }
 
-            return View(subject);
+            return View(
+                    "Display",
+                    new SubjectsDisplayVM(
+                        false,
+                        "Subject",
+                        "Are you sure you want to delete this Subject",
+                        new List<PageLinks>() { PageLinks.BackToList },
+                        subject,
+                        "Supprimer"
+                    )
+            );
         }
 
         // POST: Subjects/Delete/5
@@ -136,7 +174,7 @@ namespace CrazyBooks.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var subject = await _db.Subjects.FindAsync(id);
+            Subject subject = await _db.Subjects.FindAsync(id);
             _db.Subjects.Remove(subject);
             await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
@@ -145,6 +183,19 @@ namespace CrazyBooks.Controllers
         private bool SubjectExists(int id)
         {
             return _db.Subjects.Any(e => e.Id == id);
+        }
+
+        private SubjectsUpsertVM GetSubjectsUpsertVM(bool isCreate, Dictionary<string,SelectList­> selectLists, Subject subject = null)
+        {
+            return new SubjectsUpsertVM(
+                        isCreate,
+                        "Subject",
+                        "Subject",
+                        new List<PageLinks>() { PageLinks.BackToList },
+                        isCreate ? "Ajouter" : "Modifier",
+                        selectLists,
+                        subject
+            );
         }
     }
 }
